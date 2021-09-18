5bb2f9cfd66988f8fb9bd270f637fc5a1f78e3d2
OHS9711 <ohs9711@gmail.com>
2021-09-19 04:44:15 +0900

    add menu_type from serializer fields

--- menus/serializers.py
+++ menus/serializers.py
@@ -10,4 +10,4 @@ class MenuSerializer(serializers.ModelSerializer):
     ingredient = IngredientSerializer(many=True, read_only=True) #post시 새로 만든 ingredient필드가 영향을 미치지 않도록 read-only=True로 함
     class Meta:
         model = Menu
-        fields = ['id','name','image','price','menu_info','allergy_ingredient','branch_id','ingredient']
+        fields = ['id','name','image','price','menu_info','menu_type','allergy_ingredient','branch_id','ingredient']
